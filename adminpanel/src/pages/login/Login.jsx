6bce19a2e38e290e76931f81612b4f8cf4437ad5
Aslam <aslamkhanofficial@yahoo.com>
2022-08-31 03:18:30 +0500

    google login error

--- adminpanel/src/pages/login/Login.jsx
+++ adminpanel/src/pages/login/Login.jsx
@@ -1,5 +1,5 @@
 import "./login.scss";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   signInWithEmailAndPassword,
